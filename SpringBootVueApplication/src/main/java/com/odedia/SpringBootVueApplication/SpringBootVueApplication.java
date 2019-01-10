63b1c9128ecba70c73e3e8ffaf596817edc39e35
odedia <PpE7^%&t5yCAAx4TA9EhDbqbZPeybn66>
2019-01-11 00:44:34 +0200

    Second commit

--- SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/SpringBootVueApplication.java
+++ SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/SpringBootVueApplication.java
@@ -1,17 +1,11 @@
 package com.odedia.SpringBootVueApplication;
 
-import java.util.Collections;
 import java.util.stream.Stream;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SpringBootVueApplication {
@@ -33,21 +27,24 @@ public class SpringBootVueApplication {
         };  
     }  
 
-    // Fix the CORS errors
-    @Bean
-    public FilterRegistrationBean simpleCorsFilter() {  
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
-        CorsConfiguration config = new CorsConfiguration();  
-        config.setAllowCredentials(true); 
-        // *** URL below needs to match the Vue client URL and port ***
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); 
-        config.setAllowedMethods(Collections.singletonList("*"));  
-        config.setAllowedHeaders(Collections.singletonList("*"));  
-        source.registerCorsConfiguration("/**", config);  
-        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);  
-        return bean;  
-    }   
+// We actually don't need CORS configuration while running on PCF,
+// Because we can map both frontend and backend to the same domain name
+// and separate the applications based on path.
+    
+//    @Bean
+//    public FilterRegistrationBean simpleCorsFilter() {  
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+//        CorsConfiguration config = new CorsConfiguration();  
+//        config.setAllowCredentials(true); 
+//        // *** URL below needs to match the Vue client URL and port ***
+//        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); 
+//        config.setAllowedMethods(Collections.singletonList("*"));  
+//        config.setAllowedHeaders(Collections.singletonList("*"));  
+//        source.registerCorsConfiguration("/**", config);  
+//        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);  
+//        return bean;  
+//    }   
 
 }
 
