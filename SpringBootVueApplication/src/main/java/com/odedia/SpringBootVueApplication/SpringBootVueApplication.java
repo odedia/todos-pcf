ddb69338968685d5c3169000e985e3bb59cc5d69
odedia <PpE7^%&t5yCAAx4TA9EhDbqbZPeybn66>
2019-01-11 11:18:28 +0200

    Removed init of data

--- SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/SpringBootVueApplication.java
+++ SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/SpringBootVueApplication.java
@@ -1,11 +1,7 @@
 package com.odedia.SpringBootVueApplication;
 
-import java.util.stream.Stream;
-
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootVueApplication {
@@ -14,19 +10,6 @@ public class SpringBootVueApplication {
 		SpringApplication.run(SpringBootVueApplication.class, args);
 	}
     
-	// Bootstrap some test data into the in-memory database
-    @Bean  
-    ApplicationRunner init(TodoRepository repository) {  
-        return args -> {  
-            Stream.of("Buy milk", "Eat pizza", "Write tutorial", "Study Vue.js", "Go kayaking").forEach(name -> {  
-                    Todo todo = new Todo();  
-                    todo.setTitle(name);  
-                    repository.save(todo);  
-            });  
-            repository.findAll().forEach(System.out::println);  
-        };  
-    }  
-
 // We actually don't need CORS configuration while running on PCF,
 // Because we can map both frontend and backend to the same domain name
 // and separate the applications based on path.
