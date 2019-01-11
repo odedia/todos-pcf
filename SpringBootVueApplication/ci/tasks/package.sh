7aa054328d94a0992bc87dfd01858f4eeb88c362
odedia <PpE7^%&t5yCAAx4TA9EhDbqbZPeybn66>
2019-01-11 19:01:53 +0200

    m2 caching

--- SpringBootVueApplication/ci/tasks/package.sh
+++ SpringBootVueApplication/ci/tasks/package.sh
@@ -3,6 +3,23 @@
 set -e -u -x
 
 artifact=source-code/SpringBootVueApplication
+M2_HOME=${HOME}/.m2
+mkdir -p ${M2_HOME}
+ 
+M2_LOCAL_REPO="${ROOT_FOLDER}/.m2"
+ 
+mkdir -p "${M2_LOCAL_REPO}/repository"
+ 
+cat > ${M2_HOME}/settings.xml <<EOF
+ 
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
+      <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
+</settings>
+ 
+EOF
 
 pushd ${artifact}
   echo "Packaging JAR"
