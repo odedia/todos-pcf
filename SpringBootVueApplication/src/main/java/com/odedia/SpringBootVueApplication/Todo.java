b4a9691b48aa8cbc5b8131fdb89ab42c9eb48bc2
odedia <odedia@gmail.com>
2019-01-11 19:28:52 +0200

    Update Todo.java

--- SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/Todo.java
+++ SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/Todo.java
@@ -5,7 +5,8 @@ import lombok.*;
 import javax.persistence.Id;  
 import javax.persistence.GeneratedValue;  
 import javax.persistence.Entity;  
-  
+
+//Lombok
 @Entity  
 @Data  
 @NoArgsConstructor  
