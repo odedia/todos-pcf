8e8aa8f494d7d2e0e5d109dfbdc8158e0c1acc2b
odedia <odedia@gmail.com>
2019-01-11 19:23:07 +0200

    Update App.vue

--- VueJSClient/src/App.vue
+++ VueJSClient/src/App.vue
@@ -4,7 +4,8 @@
     <footer class="info">
       <p>This demo is based on a project written by <a href="http://evanyou.me">Evan You</a></p>
       <p>Original Vue TodoApp project is <a href="https://vuejs.org/v2/examples/todomvc.html">here</a></p>
-      <p>Modified for this tutorial by Andrew Hughes</p>
+      <p>First modified for this tutorial by Andrew Hughes</p>
+      <p>PCF modifications for this tutorial were made by Oded Shopen</p>
     </footer>
   </div>
 </template>
