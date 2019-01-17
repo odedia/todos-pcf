ad2792a0349da8841ca70997e5172bdd13d1abc6
odedia <odedia@gmail.com>
2019-01-17 12:36:16 +0200

    Update RestRepositoryConfigurator.java

--- SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/RestRepositoryConfigurator.java
+++ SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/RestRepositoryConfigurator.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 /**
  * IDs are not returned by RestRepository by default. I like them exposed so that the client can easily find
  * the ID of created and listed resources.
- * ABCD
+ * ABCDeee
  * */
 @Component
 public class RestRepositoryConfigurator implements RepositoryRestConfigurer {
