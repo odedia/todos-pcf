17b05dcfc17346f1ed1181aaad06fd7d801197c0
odedia <odedia@gmail.com>
2019-02-07 15:00:52 +0200

    Update SpringBootVueApplication.java

--- SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/SpringBootVueApplication.java
+++ SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication/SpringBootVueApplication.java
@@ -10,6 +10,7 @@ public class SpringBootVueApplication {
 		SpringApplication.run(SpringBootVueApplication.class, args);
 	}
 //aaa
+	///bbb
 	//bbb
 	// We actually don't need CORS configuration while running on PCF,
 // Because we can map both frontend and backend to the same domain name
