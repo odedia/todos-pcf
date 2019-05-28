bcab594955a7b68d45c8b7223b208787920b155a
odedia <PpE7^%&t5yCAAx4TA9EhDbqbZPeybn66>
2019-05-28 20:32:11 +0300

    Updates

--- SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication.java
+++ SpringBootVueApplication/src/main/java/com/odedia/SpringBootVueApplication.java
@@ -1,30 +1,34 @@
 package com.odedia;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
-import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @SpringBootApplication
 public class SpringBootVueApplication {
@@ -48,8 +52,23 @@ class Todo {
 	private Boolean completed = false;
 }
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
 interface TodoRepository extends JpaRepository<Todo, Long> {
+}
+
+@Slf4j
+@RepositoryEventHandler(Todo.class)
+@EnableBinding(Source.class)
+class TaskEventHandler {
+	@Autowired Source source;
+
+	@HandleBeforeSave
+	public void handleBeforeSave(Todo todo) {
+		if (todo.getCompleted()) {
+			log.info("Sending completed message for {}", todo.getTitle());
+			source.output().send(MessageBuilder.withPayload(todo.getTitle()).build());
+		}
+	}
 }
 
 @Component
