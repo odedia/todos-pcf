60e92fb232405cb94511a14485a81e790fc375dc
odedia <odedia@gmail.com>
2019-06-16 01:15:24 +0300

    Update Todos.vue

--- todo-ui/src/components/Todos.vue
+++ todo-ui/src/components/Todos.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="title">Todos V2</h1>
+    <h1 class="title">Todos</h1>
     <h1 class="email">{{userEmail}}</h1>
     <section class="todoapp">
       <div v-if="loading">
